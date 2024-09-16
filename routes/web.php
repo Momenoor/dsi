11cbfba18429f1d92dc5f6b5538cfb7a1ef65b9b
Momenoor <momen.noor@gmail.com>
2024-09-17 01:36:39 +0400

    Second

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CreditorListController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('commercial-list/{$type}', [CreditorListController::class])->name('commercial-list');
+Route::get('sokuk-list/{$type}', [CreditorListController::class])->name('sokuk-list');
+Route::get('labour-list/{$type}', [CreditorListController::class])->name('labour-list');
+
