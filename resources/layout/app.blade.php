68722f588f9237a19f663964d90f7220968f02b5
Momenoor <momen.noor@gmail.com>
2024-09-17 01:53:48 +0400

    Third

---
+++ resources/layout/app.blade.php
@@ -0,0 +1,277 @@
+
+<!DOCTYPE html>
+<html dir="ltr">
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title></title>
+    <meta name="description" />
+    <meta name="keywords" />
+    <link href="/Assets/plugins/bootstrap-jpa/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/Assets/plugins/bootstrap-jpa/css/bootstrap-theme.css" rel="stylesheet" />
+    <link href="/Assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="/Assets/plugins/owl-carousel-2.3.4/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="/Assets/plugins/plyr/plyr.min.css" rel="stylesheet" />
+    <link href="/Assets/plugins/select2/select2.min.css" rel="stylesheet" />
+    <link href="/Assets/plugins/zabuto-calendar/zabuto_calendar.min.css" rel="stylesheet" />
+    <link href="/Assets/css/jpa.min.css?v=8" rel="stylesheet" />
+    <link href="/Assets/css/addedstyles.min.css?v=12" rel="stylesheet" />
+
+
+</head>
+<body>
+
+<section id="header">
+    <div id="search-bar">
+        <a href="#" onclick="$('#search-bar').toggle(); return false;"><i class="fa fa-times"></i></a>
+        <form>
+            <input id="SearchBarInput" type="text" placeholder="Search...">
+        </form>
+    </div>
+    <nav class="navbar navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="/"><img src="/Assets/images/logo.png" /></a>
+
+                <div class="navbar-branch">
+                    <h2 class="custom-logo-title">JPA AUDITING &amp; ACCOUNTING</h2>
+                    <h2>A member of JPA International</h2>
+                    <h3>UAE, Dubai, Business Bay, Burj Khalifa Blvd, Opal Tower, 10th Floor, Office 1005</h3>
+                </div>
+
+            </div>
+            <div class="collapse navbar-collapse right-toolbar">
+                <div id="header-toolbar" class="visible-lg visible-md visible-sm">
+                    <ul>
+
+                        <li class="firsListItem"><a href="http://www.jpainternational.com/" target="_blank">JPA International</a></li>
+                        <li class=""><a href="/locations">Locate a Member</a></li>
+                        <li class=""><a href="http://application.jpaemirates.com/" target="_blank">Cases Control</a></li>
+                        <li class=""><a href="http://jpaintranet.com/" target="_blank">Members Only</a></li>
+
+                        <li class="search">
+
+                            <button type="button" class="navbar-toggle search-navbar" aria-expanded="true" onclick="$('#search-bar').toggle()">
+                                <i class="fa fa-search" style="color:white !important;"></i>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="main-navbar-container collapse navbar-collapse" id="main-navbar">
+            <div class="container">
+                <div class="visible-xs">
+                    <ul class="nav navbar-nav">
+                        <li class="search">
+                            <button type="button" class="navbar-toggle search-navbar" aria-expanded="true" onclick="$('#search-bar').toggle()">
+                                <i class="fa fa-search" style="color:white !important;"></i>
+                            </button>
+                        </li>
+
+                        <li class="firsListItem"><a href="http://www.jpainternational.com/" target="_blank">JPA International</a></li>
+                        <li class=""><a href="/locations">Locate a Member</a></li>
+                        <li class=""><a href="http://application.jpaemirates.com/" target="_blank">Cases Control</a></li>
+                        <li class=""><a href="http://jpaintranet.com/" target="_blank">Members Only</a></li>
+
+                    </ul>
+                    <hr />
+                </div>
+
+                <ul class="nav navbar-nav visible-lg">
+                    <li class="main-menu-parent-menu-item"><a data-id="21" href="/">HOME</a></li>
+                    <li class="dropdown main-menu-parent-menu-item">
+                        <a href="/en/contents/about-us" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="2">ABOUT US <i class="fa fa-angle-down"></i></a>
+
+                        <ul class="dropdown-menu">
+                            <li><a data-id="3" href="/en/our-vision">OUR VISION</a></li>
+                            <li><a data-id="4" href="/en/contents/our-mission">OUR MISSION</a></li>
+                        </ul>
+                    </li>
+                    <li class="main-menu-parent-menu-item"><a data-id="5" href="/en/contents/management-team">TEAM WORK</a></li>
+                    <li class="main-menu-parent-menu-item"><a data-id="6" href="/en/contents/services">SERVICES</a></li>
+                    <li class="dropdown main-menu-parent-menu-item">
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="8">MEDIA CENTRE <i class="fa fa-angle-down"></i></a>
+
+                        <ul class="dropdown-menu">
+                            <li><a data-id="14" href="/en/news">NEWS</a></li>
+                            <li><a data-id="15" href="/en/recognitions">RECOGNITIONS</a></li>
+                            <li><a data-id="16" href="/en/photo-gallery">PHOTO GALLERY</a></li>
+                            <li><a data-id="17" href="/en/video-gallery">VIDEO GALLERY</a></li>
+                        </ul>
+                    </li>
+                    <li class="main-menu-parent-menu-item"><a data-id="32" href="/en/contents/hot-links">HOT LINKS</a></li>
+                    <li class="main-menu-parent-menu-item"><a data-id="9" href="/contact-us">CONTACT US</a></li>
+                    <li class="dropdown main-menu-parent-menu-item">
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="29">Language <i class="fa fa-angle-down"></i></a>
+
+                        <ul class="dropdown-menu">
+                            <li><a data-id="30" href="/ar">Arabic</a></li>
+                            <li><a data-id="31" href="/">English</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav visible-md visible-sm visible-xs">
+                    <li class="main-menu-parent-menu-item"><a data-id="21" href="/">HOME</a></li>
+                    <li class="dropdown main-menu-parent-menu-item">
+                        <a href="" class="dropdown-toggle custom-dropdown-button" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="2"><i class="fa fa-angle-down"></i></a>
+                        <a class="dropdown-toggle custom-main-menu-link-item" href="/en/contents/about-us" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="2">ABOUT US </a>
+
+                        <ul class="dropdown-menu">
+                            <li><a data-id="3" href="/en/our-vision">OUR VISION</a></li>
+                            <li><a data-id="4" href="/en/contents/our-mission">OUR MISSION</a></li>
+                        </ul>
+                    </li>
+                    <li class="main-menu-parent-menu-item"><a data-id="5" href="/en/contents/management-team">TEAM WORK</a></li>
+                    <li class="main-menu-parent-menu-item"><a data-id="6" href="/en/contents/services">SERVICES</a></li>
+                    <li class="dropdown main-menu-parent-menu-item">
+                        <a href="" class="dropdown-toggle custom-dropdown-button" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="8"><i class="fa fa-angle-down"></i></a>
+                        <a class="dropdown-toggle custom-main-menu-link-item" href="" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="8">MEDIA CENTRE </a>
+
+                        <ul class="dropdown-menu">
+                            <li><a data-id="14" href="/en/news">NEWS</a></li>
+                            <li><a data-id="15" href="/en/recognitions">RECOGNITIONS</a></li>
+                            <li><a data-id="16" href="/en/photo-gallery">PHOTO GALLERY</a></li>
+                            <li><a data-id="17" href="/en/video-gallery">VIDEO GALLERY</a></li>
+                        </ul>
+                    </li>
+                    <li class="main-menu-parent-menu-item"><a data-id="32" href="/en/contents/hot-links">HOT LINKS</a></li>
+                    <li class="main-menu-parent-menu-item"><a data-id="9" href="/contact-us">CONTACT US</a></li>
+                    <li class="dropdown main-menu-parent-menu-item">
+                        <a href="" class="dropdown-toggle custom-dropdown-button" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="29"><i class="fa fa-angle-down"></i></a>
+                        <a class="dropdown-toggle custom-main-menu-link-item" href="" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" data-id="29">Language </a>
+
+                        <ul class="dropdown-menu">
+                            <li><a data-id="30" href="/ar">Arabic</a></li>
+                            <li><a data-id="31" href="/">English</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</section>
+
+
+<section>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12 text-right main-menu-breadcrumb" data-col-size="col-md-12"></div>
+        </div>
+    </div>
+</section>
+<section class="page-content">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="blog-post">
+                    <div class="page-subtitle">
+                        <h1>ABOUT US</h1>
+                        <p> </p>
+
+                    </div>
+
+                    <div class="the-text">
+                        <div style="text-align: left;"><br></div><div><br></div><div><font color="#3e3e3e" face="Tahoma"><span style="font-size: 15px;">The international Arbitrator Dr. Redha Darwish Al Rahma is Emarati Arbitrator born in UAE in 1958, he is Arbitrator, Consultant&nbsp;and expert in Finance &amp; Accounting, he has rich experience for more than 30 years since his graduation from the University of United Arab Emirates in 1982 in accounting and business administration and latest getting PhD in Financial Management&nbsp;from University&nbsp;of Paramount&nbsp;California in USA 2015, holds numerous certificates and various courses in the field of arbitration, Auditing&nbsp;and Accounting , including Diploma of international Arbitration from International Institute&nbsp; for Legal Sciences of Canada &nbsp;and Associate Degree from the International Institute of Arbitrators , he is fluent in several languages including Arabic , English and Urdu . He is certified arbitrator to the Dubai Courts and local arbitration centers, including Dubai International Arbitration Center( DIAC)&nbsp;and International Commercial Arbitration center for GCC in Bahrain and Egyptian Chamber of International and Global Arbitration including Canadian College of Legal Sciences and associate of International Institute of Arbitrators in London England( CIArb ), and associate of World Association of Arbitration in Brussels Belgium&nbsp;and Certified Registered&nbsp;Arbitrator with International Commercial Chamber in Paris, France ( ICC ).</span></font><br></div>                    </div>
+                    <div class="row">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<section id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-decor"></div>
+
+
+
+
+            <div class="col-md-4 col-xs-6 pull-right">
+                <div class="text-right">
+                    <ul class="list-unstyled list-inline">
+                        <li><a href="https://www.facebook.com/jpaemirates/" target="_blank"><img src="/Assets/images/f-icon-facebook.png" /></a></li>
+                        <li><a href="https://twitter.com/jpaemirates" target="_blank"><img src="/Assets/images/f-icon-twitter.png" /></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCRAPEbfhVsBmeajMA0dM5ag" target="_blank"><img src="/Assets/images/f-icon-youtube.png" /></a></li>
+                        <li><a href="https://linkedin.com/jpaemirates" target="_blank"><img src="/Assets/images/f-icon-linkedin.png" /></a></li>
+                    </ul>
+                    <p>© JPA AUDITING &amp; ACCOUNTING Member of JPA International <br> All rights reserved.</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="memberships">
+                    <div class="row">
+                        <div class="col-md-8">
+
+                            <div class="mcontent">
+                                <a href="/en/contents/jpa-international-member-of-the-forum-of-firms"><img src="https://media.jpaemirates.com/MenuItemImages/0/FoF_JBU25EQX7G_1SE55I4OHK.png" /></a>
+                                <a href="/en/contents/our-clients"><img src="https://media.jpaemirates.com/MenuItemImages/0/OUR CLIENTELE_3AUZ6VYRGL.PNG" /></a>
+                            </div>
+
+                        </div>
+                        <div class="col-md-4">
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div id="subscriber"></div>
+<script src="/Assets/plugins/jquery/jquery.min.js"></script>
+<script src="/Assets/plugins/bootstrap-jpa/js/bootstrap.min.js"></script>
+<script src="/Assets/plugins/owl-carousel-2.3.4/owl.carousel.min.js"></script>
+<script src="/Assets/scripts/jquery.validate.min.js"></script>
+<script src="/Assets/scripts/jquery.validate.unobtrusive.min.js"></script>
+<script src="/Assets/scripts/jquery.unobtrusive-ajax.min.js"></script>
+<script src="/Assets/plugins/plyr/plyr.min.js"></script>
+<script src="/Assets/plugins/zabuto-calendar/zabuto_calendar.min.js"></script>
+<script src="/Assets/scripts/core.js?v=21"></script>
+
+<script type="text/javascript">
+    var c = ContentDetailOperations();
+    $(document).ready(function () {
+        c.runOperations();
+    });
+</script>
+
+<script>
+    var l = LayoutOperations();
+    $(document).ready(function () {
+        l.localization.keys.add('MainMenuBreadcrumb', '/main-menu-breadcrumb');
+        l.localization.keys.add('SearchUrl', '/en/search');
+        l.localization.keys.add('Subscriber', '/subscriber');
+        l.localization.keys.add('CreateEventAttenderGroupUrl', '/attender');
+        l.localization.keys.add('CurrentPageUrl', '/en/contents/about-us');
+        l.runOperations();
+    });
+</script>
+<!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-H4ZFD89MKS"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-H4ZFD89MKS'); </script>
+
+<script>
+    const header2 = document.querySelector('.heading h2');
+    header2.innerHTML = header2.innerHTML.replace('\\n', '<br>');
+</script>
+
+</body>
+</html>
